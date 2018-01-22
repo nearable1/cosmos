b1286a6019040a57c8d91e12a36b456e16b9b7a3
nearable1 <34856765+nearable1@users.noreply.github.com>
2018-01-22 21:49:00 +0800

    2018.1.22 21:48 add annotation

--- FirstPython/src/test/connectMysql.py
+++ FirstPython/src/test/connectMysql.py
@@ -5,20 +5,23 @@ password='root'
 host='111.231.59.209'
 database='myProject'
 
+#连接mysql，相当于jdbc中的connection
 conn = mysql.connector.connect(user=name, password=password,host=host,database=database)
-print("connect succ")
 
+#获取游标
 cu = conn.cursor()
-sql = 'select * from user'
+#sql文
+sql = 'select name,password from user'
+#运行
 cu.execute(sql)
+#接收全部的返回结果行
 data = cu.fetchall()
+#遍历
 if len(data) > 0:
     for x in range(len(data)):
         print(data[x])
 
 cu.close()
+#提交
+conn.commit()
 conn.close()
-
-
-#db = connect("111.231.59.209","root","root",myProject)
-
