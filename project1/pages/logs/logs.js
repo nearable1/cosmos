fa903d5c7d02a7294071125c20a347c33d4147b7
nearable1 <liu.yigeng@ns-notepc417.nssol-sh.com>
2018-02-05 11:09:27 +0800

    小程序 加果 首次提交

---
+++ project1/pages/logs/logs.js
@@ -0,0 +1,22 @@
+//logs.js
+const util = require('../../utils/util.js')
+
+Page({
+  data: {
+    logs: [],
+    iconSize: [20, 30, 40, 50, 60, 70],
+    iconColor: [
+      'red', 'orange', 'yellow', 'green', 'rgb(0,255,255)', 'blue', 'purple'
+    ],
+    iconType: [
+      'success', 'success_no_circle', 'info', 'warn', 'waiting', 'cancel', 'download', 'search', 'clear'
+    ]
+  },
+  onLoad: function () {
+    this.setData({
+      logs: (wx.getStorageSync('logs') || []).map(log => {
+        return util.formatTime(new Date(log))
+      })
+    })
+  }
+})
