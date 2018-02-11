c3769ddab669af18b936ed7a847eedaa1e4df26b
nearable1 <liu.yigeng@ns-notepc417.nssol-sh.com>
2018-02-11 17:28:37 +0800

    样式测试

--- fruit/pages/seat/seat.js
+++ fruit/pages/seat/seat.js
@@ -1,8 +1,28 @@
+const date = new Date()
+const years = []
+const months = []
+const days = []
+
+for (let i = 1990; i <= date.getFullYear(); i++) {
+  years.push(i)
+}
+
+for (let i = 1; i <= 12; i++) {
+  months.push(i)
+}
+
+for (let i = 1; i <= 31; i++) {
+  days.push(i)
+}
+
 Page({
   data:{
+    region: ['广东省', '广州市', '海珠区'],
+    customItem: '全部',
     seats: [],
     count: 1,
-    it: "" 
+    it: "",
+    
   },
   clicktoaddseat:function(){
     this.data.it = "座位"+this.data.count
@@ -11,6 +31,12 @@ Page({
     this.setData({
       seats: this.data.seats,
       count: this.data.count
+    })
+  },
+  bindRegionChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({  
+      region: e.detail.value
     })
   }
 })
