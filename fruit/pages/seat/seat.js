4aa1ed901c31ec03d746c6f77c0db6b0068c7132
nearable1 <liu.yigeng@ns-notepc417.nssol-sh.com>
2018-02-22 17:52:45 +0800

    我的位子 名称更改

--- fruit/pages/seat/seat.js
+++ fruit/pages/seat/seat.js
@@ -1,42 +1,28 @@
-const date = new Date()
-const years = []
-const months = []
-const days = []
-
-for (let i = 1990; i <= date.getFullYear(); i++) {
-  years.push(i)
-}
-
-for (let i = 1; i <= 12; i++) {
-  months.push(i)
-}
-
-for (let i = 1; i <= 31; i++) {
-  days.push(i)
-}
-
 Page({
   data:{
-    region: ['广东省', '广州市', '海珠区'],
-    customItem: '全部',
-    seats: [],
-    count: 1,
-    it: "",
-    
+    name1:'位子1',
+    hiddenmodalput: true,
   },
-  clicktoaddseat:function(){
-    this.data.it = "座位"+this.data.count
-    this.data.seats = [this.data.it].concat(this.data.seats)
-    this.data.count += 1
+  clicktotap: function(e) {
     this.setData({
-      seats: this.data.seats,
-      count: this.data.count
+      name1: e.detail.value
     })
   },
-  bindRegionChange: function (e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
-    this.setData({  
-      region: e.detail.value
+  modalinput: function () {
+    this.setData({
+      hiddenmodalput: !this.data.hiddenmodalput,
     })
-  }
+  },
+  //取消按钮  
+  cancel: function () {
+    this.setData({
+      hiddenmodalput: true
+    });
+  },
+  //确认  
+  confirm: function () {
+    this.setData({
+      hiddenmodalput: true
+    })
+  }  
 })
