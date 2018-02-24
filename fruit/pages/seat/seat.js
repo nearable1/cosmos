4c956bc612dbc0c7576f8d8296cf73984a94c547
nearable1 <liu.yigeng@ns-notepc417.nssol-sh.com>
2018-02-24 17:50:07 +0800

    我的位子 知识点测试2

--- fruit/pages/seat/seat.js
+++ fruit/pages/seat/seat.js
@@ -1,16 +1,26 @@
+var order = ['red', 'yellow', 'blue', 'green', 'red']
 Page({
   data:{
-    name1:'位子1',
     hiddenmodalput: true,
+    list: [{ id: 0, name: '位子1' }, { id: 1, name: '位子2' }, { id: 2, name: '位子3' }, 
+      { id: 3, name: '位子3' }, { id: 4, name: '位子3' }, { id: 5, name: '位子3' },
+      { id: 6, name: '位子3' }, { id: 7, name: '位子3' }, { id: 8, name: '位子3' },
+      { id: 9, name: '位子3' }, { id: 10, name: '位子3' }, { id: 11, name: '位子3' }],
+    index: 0,
+    toView: 'green',
+    scrollTop: 100,
+    scrollLeft: 0
   },
+  //更改赋值
   bindchange: function(e) {
-    this.setData({
-      name1: e.detail.value
-    })
+    this.data.list[this.data.index].name = e.detail.value
   },
-  modalinput: function () {
+  //显示弹窗，指定选中的index
+  modalinput: function (e) {
+    console.log(e)
     this.setData({
       hiddenmodalput: !this.data.hiddenmodalput,
+      index: e.currentTarget.id
     })
   },
   //取消按钮  
@@ -19,10 +29,11 @@ Page({
       hiddenmodalput: true
     });
   },
-  //确认  
-  confirm: function () {
+  //确认并最终赋值  
+  confirm: function (e) {
     this.setData({
-      hiddenmodalput: true
+      hiddenmodalput: true,
+      list: this.data.list
     })
   }  
 })
