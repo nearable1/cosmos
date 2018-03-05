8dba1838e0855c1e5642afdadf178dfd3e298d1a
nearable1 <liu.yigeng@ns-notepc417.nssol-sh.com>
2018-03-05 09:58:18 +0800

    Merge branch 'master' of https://github.com/nearable1/cosmos

--- fruit/pages/choose/choose.js
+++ fruit/pages/choose/choose.js
@@ -7,20 +7,24 @@ Page({
   data: {
     region: [],
     date: '',
-    arrayClass: ['','1班', '2班', '3班', '4班', '5班', '6班', '7班', '8班', '9班', '10班', '11班'
-      , '12班', '13班', '14班', '15班', '16班', '17班', '18班', '19班', '20班', '21班', '22班', '23班', '24班', '25班', '26班', '27班', '28班', '29班', '30班', '31班', '32班', '33班', '34班', '35班'],
+    arrayClass: ['','1班', '2班', '3班', '4班', '5班', '6班', '7班', '8班', '9班', '10班'],
     arraySchool: ['','学校1', '学校2', '学校3', '学校4'],
     arrayGrade: ['','初一第一次座位', '初三最后一次座位', '高一第一次座位', '高三最后一次座位'],
     indexClass: 0,
     indexSchool: 0,
     indexGrade: 0,
     hiddenmodalput: true,
+    hiddenClass: true,
     //数组中的数字依次表示 picker-view 内的 picker-view-colume 选择的第几项（下标从 0 开始）
     value: [0],
     //界面显示的学校
     school: '',
     //是否需要新建学校
-    newSchcool: false
+    newSchcool: false,
+    //界面显示班级
+    className: '',
+    //是否需要新建班级
+    newClass: false
   },
   //picker-view学校选择事件
   bindSchoolChange: function (e) {
@@ -43,11 +47,11 @@ Page({
       date: e.detail.value
     })
   },
-  //班级事件
+  //picker-view班级选择事件
   bindClassChange: function(e) {
-    //console.log(e.detail.value)
+    const val = e.detail.value
     this.setData({
-      indexClass: e.detail.value
+      indexClass: val[0],
     })
   },
   //年级事件
@@ -62,6 +66,11 @@ Page({
       hiddenmodalput:true
     })
   },
+  classCancel: function() {
+    this.setData({
+      hiddenClass:true
+    })
+  },
   //弹窗右下角确定
   schoolConfirm: function(e) {
     this.setData({
@@ -74,11 +83,28 @@ Page({
       })
     }
   },
+  classConfirm: function() {
+    this.setData({
+      hiddenClass: true,
+    })
+    if (!this.data.newClass) {
+      this.setData({
+        hiddenClass: true,
+        className: this.data.arrayClass[this.data.indexClass]
+      })
+    }
+  },
   //点击显示选择学校的弹窗
   clickToChooseSchool: function() {
     //需要根据地区select出学校
     this.setData({
       hiddenmodalput: !this.data.hiddenmodalput
+    })
+  },
+  clickToChooseClass: function () {
+    //select出班级
+    this.setData({
+      hiddenClass: !this.data.hiddenClass
     })
   },
   //弹窗中输入框值改变事件
@@ -88,6 +114,15 @@ Page({
         //school: e.detail.value
         newSchool: true,
         school: e.detail.value
+      })
+    }
+  },
+  bindClassInputChange: function (e) {
+    if (e.detail.value.length >= 1) {
+      this.setData({
+        //school: e.detail.value
+        newClass: true,
+        className: e.detail.value
       })
     }
   },
@@ -103,7 +138,7 @@ Page({
         title:'请输入空白部分',
         icon: 'loading',
         mask:true,
-        duration:2000
+        duration:1000
       })
     }else {
       wx.navigateTo({
--- myseat/src/main/java/com/cosmos/controller/Action.java
+++ myseat/src/main/java/com/cosmos/controller/Action.java
@@ -1,13 +1,18 @@
 package com.cosmos.controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSON;
+import com.cosmos.entity.School;
 import com.cosmos.entity.User;
 import com.cosmos.service.UserService;
 
@@ -16,6 +21,7 @@ public class Action {
 	@Autowired
 	private UserService us;
 	
+	//登陆
 	@RequestMapping("login.do")
 	public String getUser(@RequestParam(value="userCode") String name, @RequestParam(value="userPassword") String password, Model mod) {
 		
@@ -30,4 +36,69 @@ public class Action {
 			return "error.jsp";
 		}
 	}
+	
+	//查询school--在弹窗显示
+	@RequestMapping("selectSchoolByRegion.html")
+	@ResponseBody
+	public String showSchoolByRegion(@RequestParam(value="province")String province, 
+			@RequestParam(value="city")String city, 
+			@RequestParam(value="area")String area) {
+		ArrayList<String> schoolList = us.selectSchoolByRegion(province, city, area);
+	 
+		return JSON.toJSONString(schoolList);
+	}
+	
+	//查询schoolId
+	@RequestMapping("selectSchoolId.html")
+	@ResponseBody
+	public String findSchoolId(@RequestParam(value="province")String province, @RequestParam(value="city")String city, @RequestParam(value="area")String area) {
+		String schoolId = us.selectSchoolId(province, city, area);
+	 
+		return schoolId;
+	}
+	
+	//查询classId
+	@RequestMapping("selectClassId.html")
+	@ResponseBody
+	public String findClassId(@RequestParam(value="province")String province, 
+			@RequestParam(value="city")String city, 
+			@RequestParam(value="area")String area, 
+			@RequestParam(value="year")String year, 
+			@RequestParam(value="className")String className, 
+			@RequestParam(value="times")String times) {
+		String schoolId = us.selectSchoolId(province, city, area);
+		String classId = us.selectClassId(year, className, times, schoolId);
+	 
+		return classId;
+	}
+	
+	//插入school
+	@RequestMapping("insertSchool.html")
+	@ResponseBody
+	public int addSchool(@RequestParam(value="province")String province, 
+			@RequestParam(value="city")String city, 
+			@RequestParam(value="area")String area) {
+		
+		ArrayList<String> schoolList = us.selectSchoolByRegion(province, city, area);
+		if(schoolList.size()>0) {
+			School school = new School();
+			school.setProvince(province);
+			school.setCity(city);
+			school.setArea(area);
+			
+			//使用uuid创建schoolId
+			String schoolId = UUID.randomUUID().toString();
+			school.setSchoolId(schoolId);
+			
+			us.insertSchool(school);
+			
+			//返回1表示插入成功
+			return 1;
+		}else {
+			//返回0表示已经有重名的
+			return 0;
+		}
+		
+	}
+	
 }
