50a2e8ed0af5fc4942794251cc42e63374c8a8e9
nearable1 <liu.yigeng@ns-notepc417.nssol-sh.com>
2018-03-20 14:47:02 +0800

    怪声怪气 初版

--- guaishengguaiqi/pages/video/video.js
+++ guaishengguaiqi/pages/video/video.js
@@ -1,42 +1,52 @@
 // pages/movie/movie.js
 Page({
   data: {
-    //imgUrls:'http://news.youth.cn/yl/201705/W020170503421346317624.jpg',
-    imgUrls: '../image/blackboard.png',
+    imgUrls: 'http://www.4java.cn:8080/file/image/image.png',
+    image1: 'http://www.4java.cn:8080/file/image/blackboard1.png',
+    image2: 'http://www.4java.cn:8080/file/image/blackboard2.png',
+    image3: 'http://www.4java.cn:8080/file/image/nighf.png',
+    image4: 'http://www.4java.cn:8080/file/image/pen.png',
+    image5: 'http://www.4java.cn:8080/file/image/drill.png',
+    image6: 'http://www.4java.cn:8080/file/image/pipe.png',
+    image7: 'http://www.4java.cn:8080/file/image/bottle1.png',
+    image8: 'http://www.4java.cn:8080/file/image/bottle3.png',
+    image9: 'http://www.4java.cn:8080/file/image/bottle2.png',
     movielist: ['http://www.4java.cn:8080/file/blackboard1.mp3', 'http://www.4java.cn:8080/file/blackboard2.mp3', 'http://www.4java.cn:8080/file/nighf.mp3', 'http://www.4java.cn:8080/file/writeblackboard.mp3', 'http://www.4java.cn:8080/file/docter.mp3', 'http://www.4java.cn:8080/file/pipe.mp3', 'http://www.4java.cn:8080/file/glass.mp3'],
     flag: false,
     src: '',
-    currentId: 0,
-    isFirst: true
+    currentId: 0
   },
   onReady: function (e) {
     // 使用 wx.createAudioContext 获取 audio 上下文 context
-    this.audioCtx = wx.createAudioContext('myAudio')
+    this.audioCtx = wx.createInnerAudioContext()
   },
   audio: function (e) {
     var id = e.currentTarget.id
     this.setData({
       src: this.data.movielist[id]
     })
+    //audio对象属性
+    this.audioCtx.src = this.data.src
+    this.audioCtx.loop = true
     //判断是否点击另一个方块
     if (this.data.currentId != id) {
+      console.log(this.data.currentId != id)
       this.setData({
-        flag: false
+        flag: false,
+        currentId: id
       })
     }
-    if (!this.data.flag) {
+    var flag = this.data.flag
+    if (!flag) {
       this.audioCtx.play()
       this.setData({
-        flag: true
+        flag: true,
       })
-    } else {
-      this.audioCtx.pause()
+    } else if(flag){
+      this.audioCtx.stop()
       this.setData({
         flag: false,
       })
     }
-    this.setData({
-      currentId: id
-    })
   }
 })
