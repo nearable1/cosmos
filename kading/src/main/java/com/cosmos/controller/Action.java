f1e047f6d5d003e1e9b554400957bd7929aa6ecb
nearable1 <liu.yigeng@ns-notepc417.nssol-sh.com>
2018-04-09 17:05:03 +0800

    java解密

--- kading/src/main/java/com/cosmos/controller/Action.java
+++ kading/src/main/java/com/cosmos/controller/Action.java
@@ -1,15 +1,22 @@
 package com.cosmos.controller;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSON;
 import com.cosmos.service.UserService;
+import com.cosmos.utils.SecretUtils;
 
 @Controller
 public class Action {
@@ -19,24 +26,59 @@ public class Action {
 	//根据种类获取音乐文件
 	@RequestMapping(value="getRunData.html",produces="text/html;charset=UTF-8")
 	@ResponseBody
-	public String getRunData(Model model) {
+	public String getRunData(@RequestParam(value="js_code") String js_code) {
 		HashMap<String, String> map = new HashMap<String, String>();
 
 		try {	
-//			URL url = new URL("https://api.weixin.qq.com/sns/jscode2session?"
-//					+ "appid=wx24637ac470fd8876&"
-//					+ "secret=8bfdfb8663f88494a99ca96297115306&"
-//					+ "&js_code=003rSBZo0v4Z8p1x040p0CdIZo0rSBZR"+"&grant_type=authorization_code");
-//			HttpURLConnection conn = (HttpURLConnection) url.openConnection(); 
-//			conn.setRequestMethod("POST");//修改发送方式  
+			URL url = new URL("https://api.weixin.qq.com/sns/jscode2session?"
+					+ "appid=wx24637ac470fd8876&"
+					+ "secret=d3d4668c301717c18f77f58e1e1e2b8e"
+					+ "&js_code="+js_code+"&grant_type=authorization_code");
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection(); 
+			conn.setRequestMethod("POST");//修改发送方式  
 			//{"session_key":"JiaiVZBrHkG\/dE0oQyc54A==","openid":"o1PkD5k6brfGPpdtAd4C0dRvRLDQ"} 
+			conn.setRequestProperty("Content-Type",  
+                    "application/x-www-form-urlencoded");  
+            conn.setRequestProperty("Connection", "Keep-Alive");  
+            conn.setUseCaches(false);  
+            conn.setDoOutput(true);  
   
+            // 获取响应状态  
+            if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {  
+                return "";  
+            }  
+            // 获取响应内容体  
+            String line, result = "";  
+            BufferedReader in = new BufferedReader(new InputStreamReader(  
+                    conn.getInputStream(), "utf-8"));  
+            while ((line = in.readLine()) != null) {  
+                result += line + "\n";  
+            }  
+            //"session_key":"nG5oPueAhQchSdYesE52bQ==","openid":"o1PkD5k6brfGPpdtAd4C0dRvRLDQ"
+            in.close();  
+            return result;  
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		map.put("session_key", "JiaiVZBrHkG\\/dE0oQyc54A==");
-		map.put("openid", "o1PkD5k6brfGPpdtAd4C0dRvRLDQ");
-		return JSON.toJSONString(map);
+		return "error";
+	}
+	
+	@RequestMapping(value="decodeRunData.html",produces="text/html;charset=UTF-8")
+	@ResponseBody
+	public String decodeRunData(@RequestParam(value="encryptedData") String encryptedData,
+			@RequestParam(value="iv") String iv,
+			@RequestParam(value="appid") String appId,
+			@RequestParam(value="session_key") String sessionKey) {
+		
+		String result = null;
+		try {
+			//encryptedData, iv, appId, sessionKey
+			result = SecretUtils.AES128CBCdecrypt(encryptedData, iv, appId, sessionKey);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
 	}
 }
--- wx_kading/pages/map/index.js
+++ wx_kading/pages/map/index.js
@@ -1,17 +1,15 @@
-var WXBizDataCrypt = require('../WXBizDataCrypt.js')
+
 Page({
   data: {
 
   },
   //"0134laeL17H5V41eWagL1xkbeL14laem"
-  onLoad: function (options) {
+  click: function (options) {
     var that = this;
     wx.login({
       success: function (res) {
-        console.log(res)
-        var appid = "wx33800aa282ed651b";
-        var secret = "30ffc49f6f2765faec431f2ed88b2a6a";
-        //data = {'js_code': res.code}
+        var appid = "wx24637ac470fd8876";
+        var data={"js_code":res.code}
         
         if (res.code) {
           wx.request({
@@ -20,11 +18,9 @@ Page({
             header: {
               'content-type': 'application/x-www-form-urlencoded'
             },
-            data: '',
+            data: data,
             success: function (e) {
-              console.log(e)
               var session_key = e.data.session_key;
-              console.log(session_key);
               that.getData(appid, session_key);
             },
             fail: function(e) {
@@ -60,21 +56,28 @@ Page({
         } else {
           wx.getWeRunData({
             success: function (res) {
-              console.log("appid:" + appid + "session_key:" + session_key + "encryptedData:" + res.encryptedData + "iv:" + res.iv);
-              var encryptedData = res.encryptedData;
-              var iv = res.iv;
-              //使用解密工具，链接地址：
-              //https://codeload.github.com/gwjjeff/cryptojs/zip/master
-              var pc = new WXBizDataCrypt(appid, session_key);
-              console.log(pc);
-              var data = pc.decryptData(encryptedData, iv)
-              console.log(data)
+              var data1= {'appid':appid,
+                'session_key': session_key, 'encryptedData': res.encryptedData, 'iv': res.iv}
+              wx.request({
+                url: 'https://www.4java.cn/kading/decodeRunData.html',
+                method: "POST",
+                header: {
+                  'content-type': 'application/x-www-form-urlencoded'
+                },
+                data: data1,
+                success: function (e) {
+                  console.log(e.data)
+                },
+                fail: function (e) {
+                  console.log(res.code)
+                }
+              })
+            
             },
             fail: function (res) {
               wx.showModal({
                 title: '提示',
                 content: '开发者未开通微信运动，请关注“微信运动”公众号后重试',
-                showCancel: false,
                 confirmText: '知道了'
               })
             }
