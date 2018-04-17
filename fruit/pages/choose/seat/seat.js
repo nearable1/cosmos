653e79d9e635fe08e19e50b776aac224f4c14899
nearable1 <liu.yigeng@ns-notepc417.nssol-sh.com>
2018-04-17 16:03:52 +0800

    1

--- fruit/pages/choose/seat/seat.js
+++ fruit/pages/choose/seat/seat.js
@@ -4,8 +4,12 @@ Page({
     hiddenmodalput: true,
     list: [{ id: '310', name: 'gaoe'}, { id: '410', name: '刘熠庚'}, { id: '614', name: '刘熠庚'}, { id: 'aa', name: '刘熠庚'}],
     index: 0,
+    name:''
   },
   onLoad: function() {
+    this.setData({
+      list: app.seatList
+    })
     var data = {
       'schoolId': app.schoolId,
       'classId': app.classId
@@ -19,7 +23,7 @@ Page({
       },
       success: function (e) {
         //给每个班的座位赋值-全局变量
-        console.log("get,"+data)
+        console.log(e)
       },
       fail: function (e) {
         console.log(e)
@@ -28,7 +32,7 @@ Page({
   },
   //更改赋值
   bindchange: function(e) {
-    this.data.list[this.data.index].name = e.detail.value
+    this.data.name = e.detail.value
     
   },
   //显示弹窗，指定选中的index
@@ -47,11 +51,55 @@ Page({
   },
   //确认并最终赋值  
   confirm: function (e) {
+    var that = this
     this.setData({
       hiddenmodalput: true,
       list: this.data.list
     })
     //访问后台，update数据库，加锁访问
+    var data = {
+      'schoolId': app.schoolId,
+      'classId': app.classId,
+      'name': that.data.name,
+      'seatId': that.data.index
+    }
+    var data1 = {
+      'schoolId': app.schoolId,
+      'classId': app.classId
+    }
+    wx.request({
+      url: 'https://www.4java.cn/myseat/insertSeat.do',
+      data: data,
+      method: 'POST',
+      header: {
+        "content-type": "application/x-www-form-urlencoded"
+      },
+      success: function (e) {
+        //给每个班的座位赋值-全局变量
+        console.log(e)
+        wx.request({
+          url: 'https://www.4java.cn/myseat/selectSeat.do',
+          data: data1,
+          method: 'POST',
+          header: {
+            "content-type": "application/x-www-form-urlencoded"
+          },
+          success: function (e) {
+            //给每个班的座位赋值-全局变量
+            that.setData({
+              list: e.data
+            })
+
+          },
+          fail: function (e) {
+            console.log(e)
+          }
+        })
+      },
+      fail: function (e) {
+        console.log(e)
+      }
+    })
   },
   //设置分享
   onShareAppMessage: function () {
