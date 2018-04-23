e1994cc78ef73dcccc29cc4961bf470dba0831ac
nearable1 <34856765+nearable1@users.noreply.github.com>
2018-04-23 22:09:41 +0800

    1

--- wx_kading/pages/index/index.js
+++ wx_kading/pages/index/index.js
@@ -1,54 +1,93 @@
 //index.js
 //获取应用实例
-const app = getApp()
-
+let app = getApp();
+let wechat = require("../../utils/wechat");
+let amap = require("../../utils/amap");
+let markersData = [];
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+    markers: [],
+    latitude: '',
+    longitude: '',
+    textData: {},
+    city: '',
+    markerId: 0,
+    controls: [
+      {
+        id: 0,
+        position: {
+          left: 10,
+          top: 200,
+          width: 40,
+          height: 40
+        },
+        iconPath: "/images/circle1.png",
+        clickable: true
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    ]
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  onLoad(e) {
+    amap.getRegeo()
+      .then(d => {
+        console.log(d);
+        let { name, desc, latitude, longitude } = d[0];
+        let { city } = d[0].regeocodeData.addressComponent;
+        this.setData({
+          city,
+          latitude,
+          longitude,
+          textData: { name, desc }
+        })
+      })
+      .catch(e => {
+        console.log(e);
+      })
+  },
+  bindInput() {
+    let { latitude, longitude, city } = this.data;
+    let url = `/pages/inputtip/inputtip?city=${city}&lonlat=${longitude},${latitude}`;
+    wx.navigateTo({ url });
+  },
+  makertap(e) {
+    // console.log(e);
+    let { markerId } = e;
+    let { markers } = this.data;
+    let marker = markers[markerId];
+    // console.log(marker);
+    this.showMarkerInfo(marker);
+    this.changeMarkerColor(markerId);
+  },
+  showMarkerInfo(data) {
+    let { name, address: desc } = data;
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      textData: { name, desc }
     })
+  },
+  changeMarkerColor(markerId) {
+    let { markers } = this.data;
+    markers.forEach((item, index) => {
+      item.iconPath = "/images/marker.png";
+      if (index == markerId) item.iconPath = "/images/marker_checked.png";
+    })
+    this.setData({ markers, markerId });
+  },
+  getRoute() {
+    // 起点
+    let { latitude, longitude, markers, markerId, city, textData } = this.data;
+    let { name, desc } = textData;
+    if (!markers.length) return;
+    // 终点
+    let { latitude: latitude2, longitude: longitude2 } = markers[markerId];
+    let url = `/pages/routes/routes?longitude=${longitude}&latitude=${latitude}&longitude2=${longitude2}&latitude2=${latitude2}&city=${city}&name=${name}&desc=${desc}`;
+    wx.navigateTo({ url });
+  },
+  clickcontrol(e) {
+    console.log("回到用户当前定位点");
+    let { controlId } = e;
+    let mpCtx = wx.createMapContext("map");
+    mpCtx.moveToLocation();
+  },
+  mapchange() {
+    // console.log("改变视野");
   }
 })
--- wx_kading/utils/util.js
+++ wx_kading/utils/util.js
@@ -1,19 +1,23 @@
-const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
+/**
+ * 工具类
+ */
+class Util {
+  static formatTime(date) {
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
 
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
-}
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
 
-const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : '0' + n
-}
 
-module.exports = {
-  formatTime: formatTime
-}
+    return [year, month, day].map(this.formatNumber).join('/') + ' ' + [hour, minute, second].map(this.formatNumber).join(':');
+  };
+  static formatNumber(n) {
+    n = n.toString();
+    return n[1] ? n : '0' + n;
+  };
+};
+
+module.exports = Util;
