1c93a6b90509737414e76451091a7addbe81d0b3
nearable1 <34856765+nearable1@users.noreply.github.com>
2018-04-26 22:38:45 +0800

    1

--- wx_kading/pages/weather/weather.js
+++ wx_kading/pages/weather/weather.js
@@ -1,21 +1,27 @@
-let app = getApp();
-let wechat = require("../../utils/wechat");
-let amap = require("../../utils/amap");
-
+var bmap = require('../../libs/bmap-wx.min.js');
 Page({
-  data: {
-    weather: {}
-  },
-  onLoad() {
-    amap.getWeather()
-      .then(d => {
-        console.log(d);
-        this.setData({
-          weather: d
+    data: {
+        weatherData: ''
+    },
+    onLoad: function() {
+        var that = this;
+        var BMap = new bmap.BMapWX({
+            ak: '您的ak'
         });
-      })
-      .catch(e => {
-        console.log(e);
-      })
-  }
+        var fail = function(data) {
+            console.log('fail!!!!')
+        };
+        var success = function(data) {
+            console.log('success!!!');
+            var weatherData = data.currentWeather[0];
+            weatherData = '城市：' + weatherData.currentCity + '\n' + 'PM2.5：' + weatherData.pm25 + '\n' +'日期：' + weatherData.date + '\n' + '温度：' + weatherData.temperature + '\n' +'天气：' + weatherData.weatherDesc + '\n' +'风力：' + weatherData.wind + '\n';
+            that.setData({
+                weatherData: weatherData
+            });
+        }
+        BMap.weather({
+            fail: fail,
+            success: success
+        });
+    }
 })
