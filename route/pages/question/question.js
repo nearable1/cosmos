1c6e72c5594a8ef9dbce0cf144a8a357a98282c0
liuyg <liuyg@dongzhengafc.com>
2018-10-08 13:55:29 +0800

    complete

--- route/pages/question/question.js
+++ route/pages/question/question.js
@@ -23,94 +23,94 @@ Page({
               {value: 5, name: '西瓜'},
               {value: 10, name: '菠萝'},
               {value: 15, name: '橘子'}
-      //     ],[
-      //         {value: 2, name: '郊外'},
-      //         {value: 3, name: '电影院'},
-      //         {value: 5, name: '公园'},
-      //         {value: 10, name: '商场'},
-      //         {value: 15, name: '酒吧'},
-      //         {value: 20, name: '练歌房'}
-      //     ],[
-      //     {value: 2, name: '有才气的人'},
-      //     {value: 3, name: '依赖你的人'},
-      //     {value: 5, name: '优雅的人'},
-      //     {value: 10, name: '善良的人'},
-      //     {value: 15, name: '性情豪放的人'}
-      // ],[
-      //     {value: 2, name: '猫'},
-      //     {value: 3, name: '马'},
-      //     {value: 5, name: '大象'},
-      //     {value: 10, name: '猴子'},
-      //     {value: 15, name: '狗'},
-      //     {value: 20, name: '狮子'}
-      // ],[
-      //     {value: 5, name: '游泳'},
-      //     {value: 10, name: '喝冷饮'},
-      //     {value: 15, name: '开空调'}
-      // ],[
-      //     {value: 2, name: '蛇'},
-      //     {value: 5, name: '猪'},
-      //     {value: 10, name: '老鼠'},
-      //     {value: 15, name: '苍蝇'}
-      // ],[
-      //     {value: 2, name: '悬疑推理类'},
-      //     {value: 3, name: '童话神话类'},
-      //     {value: 5, name: '自然科学类'},
-      //     {value: 10, name: '伦理道德类'},
-      //     {value: 15, name: '战争枪战类'}
-      // ],[
-      //     {value: 2, name: '打火机'},
-      //     {value: 2, name: '口红'},
-      //     {value: 3, name: '记事本'},
-      //     {value: 5, name: '纸巾'},
-      //     {value: 10, name: '手机'}
-      // ],[
-      //     {value: 2, name: '火车'},
-      //     {value: 3, name: '自行车'},
-      //     {value: 5, name: '汽车'},
-      //     {value: 10, name: '飞机'},
-      //     {value: 15, name: '步行'}
-      // ],[
-      //     {value: 2, name: '紫'},
-      //     {value: 3, name: '黑'},
-      //     {value: 5, name: '蓝'},
-      //     {value: 8, name: '白'},
-      //     {value: 12, name: '黄'},
-      //     {value: 15, name: '红'}
-      // ],[
-      //     {value: 2, name: '瑜伽'},
-      //     {value: 3, name: '自行车'},
-      //     {value: 5, name: '乒乓球'},
-      //     {value: 8, name: '拳击'},
-      //     {value: 10, name: '足球'},
-      //     {value: 15, name: '蹦极'}
-      // ],[
-      //     {value: 2, name: '湖边'},
-      //     {value: 3, name: '草原'},
-      //     {value: 5, name: '海边'},
-      //     {value: 10, name: '森林'},
-      //     {value: 15, name: '城中区'}
-      // ],[
-      //     {value: 2, name: '雪'},
-      //     {value: 3, name: '风'},
-      //     {value: 5, name: '雨'},
-      //     {value: 10, name: '雾'},
-      //     {value: 15, name: '雷电'}
-      // ],[
-      //     {value: 2, name: '七层'},
-      //     {value: 3, name: '一层'},
-      //     {value: 5, name: '二十三层'},
-      //     {value: 10, name: '十八层'},
-      //     {value: 15, name: '三十层'}
-      // ],[
-      //     {value: 1, name: '丽江'},
-      //     {value: 3, name: '拉萨'},
-      //     {value: 5, name: '昆明'},
-      //     {value: 8, name: '西安'},
-      //     {value: 10, name: '杭州'},
-      //     {value: 15, name: '北京'}
-      // ],
-      ]]
+          ],[
+              {value: 2, name: '郊外'},
+              {value: 3, name: '电影院'},
+              {value: 5, name: '公园'},
+              {value: 10, name: '商场'},
+              {value: 15, name: '酒吧'},
+              {value: 20, name: '练歌房'}
+          ],[
+          {value: 2, name: '有才气的人'},
+          {value: 3, name: '依赖你的人'},
+          {value: 5, name: '优雅的人'},
+          {value: 10, name: '善良的人'},
+          {value: 15, name: '性情豪放的人'}
+      ],[
+          {value: 2, name: '猫'},
+          {value: 3, name: '马'},
+          {value: 5, name: '大象'},
+          {value: 10, name: '猴子'},
+          {value: 15, name: '狗'},
+          {value: 20, name: '狮子'}
+      ],[
+          {value: 5, name: '游泳'},
+          {value: 10, name: '喝冷饮'},
+          {value: 15, name: '开空调'}
+      ],[
+          {value: 2, name: '蛇'},
+          {value: 5, name: '猪'},
+          {value: 10, name: '老鼠'},
+          {value: 15, name: '苍蝇'}
+      ],[
+          {value: 2, name: '悬疑推理类'},
+          {value: 3, name: '童话神话类'},
+          {value: 5, name: '自然科学类'},
+          {value: 10, name: '伦理道德类'},
+          {value: 15, name: '战争枪战类'}
+      ],[
+          {value: 2, name: '打火机'},
+          {value: 2, name: '口红'},
+          {value: 3, name: '记事本'},
+          {value: 5, name: '纸巾'},
+          {value: 10, name: '手机'}
+      ],[
+          {value: 2, name: '火车'},
+          {value: 3, name: '自行车'},
+          {value: 5, name: '汽车'},
+          {value: 10, name: '飞机'},
+          {value: 15, name: '步行'}
+      ],[
+          {value: 2, name: '紫'},
+          {value: 3, name: '黑'},
+          {value: 5, name: '蓝'},
+          {value: 8, name: '白'},
+          {value: 12, name: '黄'},
+          {value: 15, name: '红'}
+      ],[
+          {value: 2, name: '瑜伽'},
+          {value: 3, name: '自行车'},
+          {value: 5, name: '乒乓球'},
+          {value: 8, name: '拳击'},
+          {value: 10, name: '足球'},
+          {value: 15, name: '蹦极'}
+      ],[
+          {value: 2, name: '湖边'},
+          {value: 3, name: '草原'},
+          {value: 5, name: '海边'},
+          {value: 10, name: '森林'},
+          {value: 15, name: '城中区'}
+      ],[
+          {value: 2, name: '雪'},
+          {value: 3, name: '风'},
+          {value: 5, name: '雨'},
+          {value: 10, name: '雾'},
+          {value: 15, name: '雷电'}
+      ],[
+          {value: 2, name: '七层'},
+          {value: 3, name: '一层'},
+          {value: 5, name: '二十三层'},
+          {value: 10, name: '十八层'},
+          {value: 15, name: '三十层'}
+      ],[
+          {value: 1, name: '丽江'},
+          {value: 3, name: '拉萨'},
+          {value: 5, name: '昆明'},
+          {value: 8, name: '西安'},
+          {value: 10, name: '杭州'},
+          {value: 15, name: '北京'}
+      ],
+      ]
   },
   onLoad: function () {
     this.setData({
